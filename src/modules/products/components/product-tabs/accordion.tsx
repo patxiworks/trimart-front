9d2847b11e3bf5689a4c1c69ea742fa94d6ae73e
Patrick Enaholo <patxiworks@gmail.com>
2024-03-10 13:39:43 +0100

    fixed AccordionPrimitive error

--- src/modules/products/components/product-tabs/accordion.tsx
+++ src/modules/products/components/product-tabs/accordion.tsx
@@ -42,8 +42,8 @@ const Item: React.FC<AccordionItemProps> = ({
   triggerable,
   ...props
 }) => {
-  /* @ts-expect-error */
   return (
+    /* @ts-expect-error */
     <AccordionPrimitive.Item
       {...props}
       className={clx(
@@ -52,12 +52,14 @@ const Item: React.FC<AccordionItemProps> = ({
         className
       )}
     >
+      {/* @ts-expect-error */}
       <AccordionPrimitive.Header className="px-1">
         <div className="flex flex-col">
           <div className="flex w-full items-center justify-between">
             <div className="flex items-center gap-4">
               <Text className="text-ui-fg-subtle text-sm">{title}</Text>
             </div>
+            {/* @ts-expect-error */}
             <AccordionPrimitive.Trigger>
               {customTrigger || <MorphingTrigger />}
             </AccordionPrimitive.Trigger>
@@ -69,6 +71,7 @@ const Item: React.FC<AccordionItemProps> = ({
           )}
         </div>
       </AccordionPrimitive.Header>
+      {/* @ts-expect-error */}
       <AccordionPrimitive.Content
         forceMount={forceMountContent}
         className={clx(
